746c8eb7253a3a8afd2e68f8cb4b3ba156c16122
Dat Pham <dat.pham@tuni.fi>
2022-09-16 10:57:47 +0300

    Draw a rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -43,21 +43,31 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = {  
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 
 	Shader myShader("../../../src/shaders/shader.vertex", "../../../src/shaders/shader.fragment");
 
-	unsigned int VBO,VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -70,8 +80,10 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		myShader.use();
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		/*glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);*/
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
